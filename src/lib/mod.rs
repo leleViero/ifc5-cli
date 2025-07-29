337ace5930a01e7bc46857281f3f36a3512fb189
Daniele Viero <daniele.viero@gruner.ch>
2025-07-29 23:01:32 +0200

    main colsole class

---
+++ src/lib/mod.rs
@@ -0,0 +1,6 @@
+// re-exporting modules for library functionality
+pub mod parser;
+pub mod transformer;
+pub mod writer;
+pub mod schema;
+pub mod models;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+/// CLI options for ifcx-gen
+#[derive(Parser, Debug)]
+#[command(name = "ifcx-gen")]
+#[command(author = "Your Name <you@example.com>")]
+#[command(version = "0.1.0")]
+#[command(about = "Converts IFC 4.3 STEP files to IFC5 .ifcx format", long_about = None)]
+struct Cli {
+    /// Path to the input .ifc file
+    #[arg(short, long)]
+    input: String,
+
+    /// Path to the output .ifcx file
+    #[arg(short, long)]
+    output: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse CLI arguments into `Cli` struct
+    let args = Cli::parse();
+
+    println!("Input file: {}", args.input);
+    println!("Output file: {}", args.output);
 }
