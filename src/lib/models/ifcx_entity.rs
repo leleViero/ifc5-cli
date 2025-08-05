8a8e1c522856eb9698164bcc91951e9a2dbbd1f7
Daniele Viero <daniele.viero@gruner.ch>
2025-08-05 21:44:43 +0200

    writer complete

--- src/lib/models/ifcx_entity.rs
+++ src/lib/models/ifcx_entity.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 
 /// A structured entity ready to be serialized as part of a .ifcx file
 #[derive(Debug, Serialize)]
+#[derive(Clone)]
 pub struct IfcxEntity {
     pub path: String,
     #[serde(rename = "type")] // ensures the output field is "type", not entity_type, to match the .ifcx schema
